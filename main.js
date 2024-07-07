562d9f08772f6cc6ca4150a417e2ef35012c8983
Acer <telecomonline68@gmail.com>
2024-07-07 17:13:09 +0400

    new

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let depositButton = document.querySelector('#deposit');
-    let withdrawButton = document.querySelector('#withdraw');
-    let moneyInput = document.querySelector('#money');
-    let userBalance = document.querySelector('#userBalance');
+let depositButton = document.querySelector('#deposit');
+let withdrawButton = document.querySelector('#withdraw');
+let moneyInput = document.querySelector('#money');
+let userBalance = document.querySelector('#userBalance');
 
-    let balance = 0;
+let balance = 0;
 
-    function updateBalance() {
-        userBalance.textContent = balance;
+function updateBalance() {
+    userBalance.textContent = balance;
+}
+
+depositButton.addEventListener('click', function() {
+    let amount = Number(moneyInput.value);
+    if (amount > 0) {
+        balance += amount;
+        updateBalance();
+    } else {
+        alert("Please enter a valid amount to deposit.");
     }
+    moneyInput.value = '';
+});
 
-    depositButton.addEventListener('click',function() {
-        let amount = Number(moneyInput.value);
-        if (amount > 0) {
-            balance += amount;
+withdrawButton.addEventListener('click', function() {
+    let amount = Number(moneyInput.value);
+    if (amount > 0) {
+        if (balance >= amount) {
+            balance -= amount;
             updateBalance();
         } else {
-            alert("Please enter a valid amount to deposit.");
+            alert("Insufficient balance.");
         }
-        moneyInput.value = '';
-    });
-
-    withdrawButton.addEventListener('click', function() {
-        let amount = Number(moneyInput.value);
-        if (amount > 0) {
-            if (balance >= amount) {
-                balance -= amount;
-                updateBalance();
-            } else {
-                alert("Insufficient balance.");
-            }
-        } else {
-            alert("Please enter a valid amount to withdraw.");
-        }
-        moneyInput.value = '';
-    });
-
-    updateBalance();
+    } else {
+        alert("Please enter a valid amount to withdraw.");
+    }
+    moneyInput.value = '';
 });
+
+updateBalance();
